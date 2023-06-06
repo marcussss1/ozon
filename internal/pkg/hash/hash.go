036e32105d3447a28ce21c87f0cae4f0aba1865a
marcussss1 <danilalakalash60@gmail.com>
2023-06-06 23:04:21 +0300

    argon2 -> sha256

--- internal/pkg/hash/hash.go
+++ internal/pkg/hash/hash.go
@@ -1,19 +1,18 @@
 package hash
 
 import (
-	"encoding/base64"
-	"golang.org/x/crypto/argon2"
+	"crypto/sha256"
+	"encoding/hex"
 	"regexp"
 )
 
-var salt = []byte{0x5C, 0x72, 0x69, 0x67, 0x61, 0x64, 0x65}
-
 func Hash(originalUrl string) string {
-	abbreviatedUrlHash := argon2.IDKey([]byte(originalUrl), salt, 1, 1, 1, 32)
-	abbreviatedUrl := base64.StdEncoding.EncodeToString(abbreviatedUrlHash)
+	h := sha256.New()
+	h.Write([]byte(originalUrl))
 
+	hash := hex.EncodeToString(h.Sum(nil))
 	re := regexp.MustCompile("[^a-zA-Z0-9_]")
-	abbreviatedUrl = re.ReplaceAllString(abbreviatedUrl, "")
+	hash = re.ReplaceAllString(hash, "")
 
-	return abbreviatedUrl[:10]
+	return hash[:10]
 }
